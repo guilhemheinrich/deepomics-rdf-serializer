4661422636985b8c204fb401688c3276b3905b10
guilhemheinrich <zezongo@gmail.com>
2023-05-02 20:23:37 +0200

    Updated ontology parser

--- index.ts
+++ index.ts
@@ -1,3 +1,13 @@
 import RDFserializer_service from "./src/Core/RDFserializer_service";
+import gqlRDF from "./src/Core/processRdf";
 
-const rdf_service = RDFserializer_service.getInstance()
+let juip = {
+    async main() {
+        const rdf_service = await RDFserializer_service.getInstance()
+        const mappings = rdf_service.mapping_templater()
+        console.log("mappings")
+        console.log(mappings)
+    }
+}
+
+juip.main()
